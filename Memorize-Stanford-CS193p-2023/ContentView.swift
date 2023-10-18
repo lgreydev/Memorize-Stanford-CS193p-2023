4c741feb145d3715026515f53b540b6277b40ef5
Sergey Lukaschuk <s.lukaschuk@yahoo.com>
2023-10-18 16:40:15 -0400

    Initial Commit

---
+++ Memorize-Stanford-CS193p-2023/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Memorize-Stanford-CS193p-2023
+//
+//  Created by Sergey Lukaschuk on 2023-10-18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Memorize-Stanford-CS193p-2023/Memorize_Stanford_CS193p_2023App.swift
@@ -0,0 +1,17 @@
+//
+//  Memorize_Stanford_CS193p_2023App.swift
+//  Memorize-Stanford-CS193p-2023
+//
+//  Created by Sergey Lukaschuk on 2023-10-18.
+//
+
+import SwiftUI
+
+@main
+struct Memorize_Stanford_CS193p_2023App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
