b29ab3ff33863a4f181955566b8e24fa63e98173
Sergey Lukaschuk <s.lukaschuk@yahoo.com>
2023-10-19 09:00:58 -0400

    Side of the card

--- Memorize-Stanford-CS193p-2023/ContentView.swift
+++ Memorize-Stanford-CS193p-2023/ContentView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         HStack {
-            CardView()
+            CardView(isFaceUp: true)
             CardView()
             CardView()
             CardView()
@@ -21,16 +21,23 @@ struct ContentView: View {
 }
 
 struct CardView: View {
+    var isFaceUp: Bool = false
+    
     var body: some View {
         ZStack {
-            RoundedRectangle(cornerRadius: 12)
-                .foregroundStyle(.white)
-            
-            RoundedRectangle(cornerRadius: 12)
-                .strokeBorder(lineWidth: 2)
-
-            Text("👻")
-                .font(.largeTitle)
+            if isFaceUp {
+                RoundedRectangle(cornerRadius: 12)
+                    .foregroundStyle(.white)
+                
+                RoundedRectangle(cornerRadius: 12)
+                    .strokeBorder(lineWidth: 2)
+                
+                Text("👻")
+                    .font(.largeTitle)
+            } else {
+                RoundedRectangle(cornerRadius: 12)
+                    .foregroundStyle(.orange)
+            }
         }
     }
 }
