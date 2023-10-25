0a5e322679899b3a50e0982655d4db496dfd6e06
Sergey Lukaschuk <s.lukaschuk@yahoo.com>
2023-10-25 05:53:52 -0400

    Buttons for adding and removing cards

--- Memorize-Stanford-CS193p-2023/ContentView.swift
+++ Memorize-Stanford-CS193p-2023/ContentView.swift
@@ -10,15 +10,40 @@ import SwiftUI
 struct ContentView: View {
     let emojis = ["🎃", "👻", "🎃", "😈", "😱", "🧙‍♂️", "🧌", "🧟‍♀️", "🕸️", "🕷️", "🍭", "💀"]
     
+    @State var cardCount = 4
+    
     var body: some View {
-        HStack {
-           
-            ForEach(emojis.indices, id:\.self) { index in
-                CardView(content: emojis[index])
+        VStack {
+            HStack {
+                ForEach(0..<cardCount, id:\.self) { index in
+                    CardView(content: emojis[index])
+                }
             }
+            .foregroundStyle(.orange)
+            
+            HStack {
+                Button(action: {
+                    if  cardCount > 1 {
+                        cardCount -= 1
+                    }
+                }, label: {
+                    Image(systemName: "rectangle.stack.fill.badge.minus")
+                })
+                
+                Spacer()
+                
+                Button(action: {
+                    if  cardCount < emojis.count {
+                        cardCount += 1
+                    }
+                }, label: {
+                    Image(systemName: "rectangle.stack.fill.badge.plus")
+                })
+            }
+            .imageScale(.large)
+            .font(.largeTitle)
         }
         .padding()
-        .foregroundStyle(.orange)
     }
 }
 
