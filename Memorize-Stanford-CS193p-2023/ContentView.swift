f2519d0dd6e6ff118e6418060bf486f9e934a6fa
Sergey Lukaschuk <s.lukaschuk@yahoo.com>
2023-10-26 11:31:47 -0400

    fix

--- Memorize-Stanford-CS193p-2023/ContentView.swift
+++ Memorize-Stanford-CS193p-2023/ContentView.swift
@@ -24,7 +24,7 @@ struct ContentView: View {
     }
     
     var cards: some View {
-        LazyVGrid(columns: [GridItem(.adaptive(minimum: 120))]) {
+        LazyVGrid(columns: [GridItem(.adaptive(minimum: 80))]) {
             ForEach(0..<cardCount, id:\.self) { index in
                 CardView(content: emojis[index])
                     .aspectRatio(2/3, contentMode: .fit)
