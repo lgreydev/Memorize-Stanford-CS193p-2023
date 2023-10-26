b46cf433efd09abe58ba2177cbdbcd632ad53d3c
Sergey Lukaschuk <s.lukaschuk@yahoo.com>
2023-10-26 11:17:59 -0400

    Scroll

--- Memorize-Stanford-CS193p-2023/ContentView.swift
+++ Memorize-Stanford-CS193p-2023/ContentView.swift
@@ -14,7 +14,9 @@ struct ContentView: View {
     
     var body: some View {
         VStack {
-            cards
+            ScrollView {
+                cards
+            }
             Spacer()
             cardCountAdjustment
         }
@@ -25,6 +27,7 @@ struct ContentView: View {
         LazyVGrid(columns: [GridItem(.adaptive(minimum: 120))]) {
             ForEach(0..<cardCount, id:\.self) { index in
                 CardView(content: emojis[index])
+                    .aspectRatio(2/3, contentMode: .fit)
             }
         }
         .foregroundStyle(.orange)
