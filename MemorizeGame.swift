ec976500df7d3a41e8d34abe0cd19895202b6b79
Sergey Lukaschuk <s.lukaschuk@yahoo.com>
2023-11-04 06:42:55 -0400

    Init a model for the game

--- MemorizeGame.swift
+++ MemorizeGame.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+
+struct MemoryGame<CardContent> {
+    var cards: Array<Card>
+    
+    func choose(card: Card) {
+        
+    }
+    
+    struct Card {
+        var isFaceUp: Bool
+        var isMatched: Bool
+        var content: CardContent
+    }
+}
