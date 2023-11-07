30f371eb2ad06f3c11d0d4c5aadabb540dc3c4b5
Sergey Lukaschuk <s.lukaschuk@yahoo.com>
2023-11-07 09:51:08 -0500

    Customizing the appearance of content in cards

--- Memorize-Stanford-CS193p-2023/EmojiMemoryGameView.swift
+++ Memorize-Stanford-CS193p-2023/EmojiMemoryGameView.swift
@@ -18,10 +18,11 @@ struct EmojiMemoryGameView: View {
     }
     
     var cards: some View {
-        LazyVGrid(columns: [GridItem(.adaptive(minimum: 85))]) {
+        LazyVGrid(columns: [GridItem(.adaptive(minimum: 85), spacing: 0)], spacing: 0) {
             ForEach(viewModel.cards.indices, id:\.self) { index in
-                CardView(card: viewModel.cards[index])
+                CardView(viewModel.cards[index])
                     .aspectRatio(2/3, contentMode: .fit)
+                    .padding(4)
             }
         }
         .foregroundStyle(.orange)
@@ -31,13 +32,20 @@ struct EmojiMemoryGameView: View {
 struct CardView: View {
     let card: MemoryGame<String>.Card
     
+    init(_ card: MemoryGame<String>.Card) {
+        self.card = card
+    }
+    
     var body: some View {
         ZStack {
             let base = RoundedRectangle(cornerRadius: 12)
             Group {
                 base.fill(.white)
                 base.strokeBorder(lineWidth: 2)
-                Text(card.content).font(.largeTitle)
+                Text(card.content)
+                    .font(.system(size: 200))
+                    .minimumScaleFactor(0.01)
+                    .aspectRatio(1, contentMode: .fit)
             }
             .opacity(card.isFaceUp ? 1 : 0)
             base.fill()
