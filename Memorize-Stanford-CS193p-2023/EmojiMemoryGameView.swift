e5112f0c5d0cafa7fd9d15c7a4416d469d95989e
Serhii Lukashchuk <s.lukaschuk@yahoo.com>
2023-11-15 05:53:30 -0500

    Fix

--- Memorize-Stanford-CS193p-2023/EmojiMemoryGameView.swift
+++ Memorize-Stanford-CS193p-2023/EmojiMemoryGameView.swift
@@ -29,13 +29,9 @@ struct EmojiMemoryGameView: View {
     var cards: some View {
         LazyVGrid(columns: [GridItem(.adaptive(minimum: 85), spacing: 0)], spacing: 0) {
             ForEach(viewModel.cards) { card in
-                VStack {
-                    CardView(card)
-                        .aspectRatio(2/3, contentMode: .fit)
-                        .padding(4)
-                    
-                    Text(card.id)
-                }
+                CardView(card)
+                    .aspectRatio(2/3, contentMode: .fit)
+                    .padding(4)
             }
         }
         .foregroundStyle(.orange)
