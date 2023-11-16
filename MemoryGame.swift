9c6f521e41180e6ba4ffe1f80c16fa986db71f0a
Serhii Lukashchuk <s.lukaschuk@yahoo.com>
2023-11-16 10:56:17 -0500

    Logic for selected card

--- MemoryGame.swift
+++ MemoryGame.swift
@@ -20,9 +20,27 @@ struct MemoryGame<CardContent> where CardContent: Equatable {
         }
     }
     
+    var indexOfTheOneAndOnlyFaceUpCard: Int?
+    
     mutating func choose(_ card: Card) {
         if let chosenIndex = cards.firstIndex(where: { $0.id == card.id }) {
-            cards[chosenIndex].isFaceUp.toggle()
+            if !cards[chosenIndex].isFaceUp && !cards[chosenIndex].isMatched {
+                
+                if let potentialMatchIndex = indexOfTheOneAndOnlyFaceUpCard {
+                    if cards[chosenIndex].content == cards[potentialMatchIndex].content {
+                        cards[chosenIndex].isMatched = true
+                        cards[potentialMatchIndex].isMatched = true
+                    }
+                    indexOfTheOneAndOnlyFaceUpCard = nil
+                } else {
+                    for index in cards.indices {
+                        cards[index].isFaceUp = false
+                    }
+                    indexOfTheOneAndOnlyFaceUpCard = chosenIndex
+                }
+                
+                cards[chosenIndex].isFaceUp = true
+            }
         }
     }
     
@@ -32,7 +50,7 @@ struct MemoryGame<CardContent> where CardContent: Equatable {
     }
     
     struct Card: Equatable, Identifiable, CustomDebugStringConvertible {
-        var isFaceUp: Bool = true
+        var isFaceUp: Bool = false
         var isMatched: Bool = false
         var content: CardContent
         
