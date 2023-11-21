7d9fc17c6f4ed905cb7637df42167bef418b4208
Serhii Lukashchuk <s.lukaschuk@yahoo.com>
2023-11-20 19:26:52 -0500

    Change directories

--- Memorize-Stanford-CS193p-2023/MemoryGame.swift
+++ Memorize-Stanford-CS193p-2023/MemoryGame.swift
@@ -2,7 +2,67 @@
 //  MemoryGame.swift
 //  Memorize-Stanford-CS193p-2023
 //
-//  Created by Sergey Lukaschuk on 2023-11-20.
+//  Created by Sergey Lukaschuk on 2023-11-04.
 //
 
 import Foundation
+
+struct MemoryGame<CardContent> where CardContent: Equatable {
+    private(set) var cards: Array<Card>
+    
+    init(numberOfPairsOfCards: Int, cardContentFactory: (Int) -> CardContent) {
+        cards = Array<Card>()
+        // add numberOfPairsOfCards x 2 cards
+        for pairIndex in 0..<max(1, numberOfPairsOfCards) {
+            let content: CardContent = cardContentFactory(pairIndex)
+            cards.append(Card(content: content, id: "\(pairIndex+1)a"))
+            cards.append(Card(content: content, id: "\(pairIndex+1)b"))
+        }
+    }
+    
+    var indexOfTheOneAndOnlyFaceUpCard: Int? {
+        get { cards.indices.filter { index in cards[index].isFaceUp }.only }
+        set { cards.indices.forEach { cards[$0].isFaceUp = (newValue == $0) }
+        }
+    }
+    
+    mutating func choose(_ card: Card) {
+        if let chosenIndex = cards.firstIndex(where: { $0.id == card.id }) {
+            if !cards[chosenIndex].isFaceUp && !cards[chosenIndex].isMatched {
+                
+                if let potentialMatchIndex = indexOfTheOneAndOnlyFaceUpCard {
+                    if cards[chosenIndex].content == cards[potentialMatchIndex].content {
+                        cards[chosenIndex].isMatched = true
+                        cards[potentialMatchIndex].isMatched = true
+                    }
+                } else {
+                    indexOfTheOneAndOnlyFaceUpCard = chosenIndex
+                }
+                
+                cards[chosenIndex].isFaceUp = true
+            }
+        }
+    }
+    
+    mutating func shuffle() {
+        self.cards.shuffle()
+        print(cards) // FIXME: print
+    }
+    
+    struct Card: Equatable, Identifiable, CustomDebugStringConvertible {
+        var isFaceUp: Bool = false
+        var isMatched: Bool = false
+        var content: CardContent
+        
+        var id: String
+        var debugDescription: String {
+            return "\(id): \(content) \(isFaceUp ? "up" : "down") \(isMatched ? "matched" : "")"
+        }
+    }
+}
+
+extension Array {
+    var only: Element? {
+        return count == 1 ? first : nil
+    }
+}
