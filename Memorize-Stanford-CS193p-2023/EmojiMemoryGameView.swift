a297f65653f704c712e4f9c1fc3fbe25c380f540
Serhii Lukashchuk <s.lukaschuk@yahoo.com>
2023-11-22 12:00:31 -0500

    Aspect Grid

--- Memorize-Stanford-CS193p-2023/EmojiMemoryGameView.swift
+++ Memorize-Stanford-CS193p-2023/EmojiMemoryGameView.swift
@@ -24,45 +24,15 @@ struct EmojiMemoryGameView: View {
     }
 
     private var cards: some View {
-        GeometryReader { geometry in
-            let gridItemSize = gridItemWidthThatFits(
-                count: viewModel.cards.count,
-                size: geometry.size,
-                atAspectRatio: aspectRatio)
-            
-            LazyVGrid(columns: [GridItem(.adaptive(minimum: gridItemSize), spacing: 0)], spacing: 0) {
-                ForEach(viewModel.cards) { card in
-                    CardView(card)
-                        .aspectRatio(aspectRatio, contentMode: .fit)
-                        .padding(4)
-                        .onTapGesture {
-                            viewModel.choose(card)
-                        }
+        AspectVGrid(items: viewModel.cards, aspectRatio: aspectRatio) { card in
+            CardView(card)
+                .padding(4)
+                .onTapGesture {
+                    viewModel.choose(card)
                 }
-            }
         }
-        .foregroundStyle(.orange)
+        .foregroundStyle(Color.orange)
     }
-    
-    private func gridItemWidthThatFits(
-        count: Int,
-        size: CGSize,
-        atAspectRatio aspectRatio: CGFloat) -> CGFloat {
-            let count = CGFloat(count)
-            var columnCount = 1.0
-            
-            repeat {
-                let width = size.width / columnCount
-                let hight = width / aspectRatio
-                
-                let rowCount = (count / columnCount).rounded(.up)
-                if rowCount * hight < size.height {
-                    return (size.width / columnCount).rounded(.down)
-                }
-                columnCount += 1
-            } while columnCount < count
-            return min(size.width / count, size.height * aspectRatio).rounded(.down)
-        }
 }
 
 struct CardView: View {
