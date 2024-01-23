5bdf276d1b64a0a41a8a545602a488cff0ba3056
Serhii Lukashchuk <s.lukaschuk@yahoo.com>
2024-01-22 21:34:15 -0500

    refactor

--- Memorize-Stanford-CS193p-2023/Cardify.swift
+++ Memorize-Stanford-CS193p-2023/Cardify.swift
@@ -13,12 +13,10 @@ struct Cardify: ViewModifier {
     func body(content: Content) -> some View {
         ZStack {
             let base = RoundedRectangle(cornerRadius: Constants.cornerRadius)
-            Group {
-                base.fill(.white)
-                base.strokeBorder(lineWidth: Constants.lineWidth)
-                content
-            }
-            .opacity(isFaceUp ? 1 : 0)
+            base.strokeBorder(lineWidth: Constants.lineWidth)
+                .background(base.fill(.white))
+                .overlay(content)
+                .opacity(isFaceUp ? 1 : 0)
             base.fill()
                 .opacity(isFaceUp ? 0: 1)
         }
